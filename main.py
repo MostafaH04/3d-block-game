867606895a23a93d87aee13cd07792ce7a187381
Mostafa Hussein <tnt.empire00@gmail.com>
2021-08-28 22:31:07 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import pygame
+from pygame import color
+from pygame import draw
+from pygame.constances import MOUSEBUTTONDOWN, MOUSEBUTTONUP, MOUSEMOTION
+from pygame.locals import KEYDOWN, QUIT
+
+
